a0b4fedb98e68761e90a9683075808ede21124ea
Jakob Fassunge <jakob.fassunge@esentri.com>
2025-01-21 16:29:11 +0100

    added Publisher

--- src/main/java/com/esentri/ddd/example/bicyclerent/outbound/ApplicationDomainEventPublisher.java
+++ src/main/java/com/esentri/ddd/example/bicyclerent/outbound/ApplicationDomainEventPublisher.java
@@ -1,26 +1,22 @@
 package com.esentri.ddd.example.bicyclerent.outbound;
 
-import org.springframework.context.ApplicationEventPublisher;
-import org.springframework.stereotype.Service;
-
 import io.domainlifecycles.domain.types.DomainEvent;
 import io.domainlifecycles.events.publish.DomainEventPublisher;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.stereotype.Service;
 
-/**
- * ApplicationDomainEventPublisher
- */
+/** ApplicationDomainEventPublisher */
 @Service
 @RequiredArgsConstructor
 public class ApplicationDomainEventPublisher implements DomainEventPublisher {
-    /**
-     * @see ApplicationEventPublisher
-     */
-    private final ApplicationEventPublisher applicationEventPublisher;
+  /**
+   * @see ApplicationEventPublisher
+   */
+  private final ApplicationEventPublisher applicationEventPublisher;
 
-    @Override
-    public void publish(DomainEvent domainEvent) {
-        applicationEventPublisher.publishEvent(domainEvent);
-    }
-
+  @Override
+  public void publish(DomainEvent domainEvent) {
+    applicationEventPublisher.publishEvent(domainEvent);
+  }
 }
