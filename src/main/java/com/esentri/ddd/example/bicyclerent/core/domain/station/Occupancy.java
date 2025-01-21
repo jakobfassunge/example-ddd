55eec758fd525652db3a2c4ab4d5abec65e4f43b
Jakob Fassunge <jakob.fassunge@esentri.com>
2025-01-21 11:28:04 +0100

    update Occupancy

--- src/main/java/com/esentri/ddd/example/bicyclerent/core/domain/station/Occupancy.java
+++ src/main/java/com/esentri/ddd/example/bicyclerent/core/domain/station/Occupancy.java
@@ -1,6 +1,5 @@
 package com.esentri.ddd.example.bicyclerent.core.domain.station;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 import io.domainlifecycles.domain.types.ValueObject;
